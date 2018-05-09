166d5cf8fbe97bd8127a5f36891ce7bbaa3b426b
aravindhoni <aravindarjun48@gmail.com>
2018-05-09 16:36:30 +0530

    ebay_appexercise

--- search_product/search_prod.py
+++ search_product/search_prod.py
@@ -3,13 +3,11 @@ from appium import webdriver
 from selenium import webdriver
 from appium.webdriver.webdriver import WebDriver as wd
 from appium.webdriver.common.touch_action import TouchAction
-import time
 DEFAUL_WAIT_TIME = 20
 class Searchproduct:
     def __init__(self,driver,objsuite):
         self.objsuite = objsuite
         self.driver = driver
-        #self.webdriv = webdriv
     def search_product(self,productname):
         """
             :Step 1: Click on search bar
@@ -28,7 +26,6 @@ class Searchproduct:
                 if elem!=None:
                     elem.send_keys(productname)
                     elem1 = self.driver.custom_driver_wait(XPATH.search_bar_after_click)
-                    #time.sleep(10)
                     elem2 = self.driver.click(XPATH.suggest_list,DEFAUL_WAIT_TIME)
                     elem3 = self.driver.selectXpath(XPATH.pop_up,DEFAUL_WAIT_TIME)
                     elem4 = self.driver.click(XPATH.first_search_result,DEFAUL_WAIT_TIME)
